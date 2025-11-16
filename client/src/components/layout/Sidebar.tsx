33d923d86aedfa0d3ed08c71463ca5c375a3fd17
Rainyel <devrainyel@gmail.com>
2025-11-16 23:05:00 +0800

    feat: sidebar toggle and transition

---
+++ client/src/components/layout/Sidebar.tsx
@@ -0,0 +1,61 @@
+import { House, Users, Camera, MessagesSquare, CirclePlus } from 'lucide-react';
+import { useState } from 'react';
+
+const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
+  const sidebarLinks = [
+    { label: 'Feed', icon: <House /> },
+    { label: 'Connections', icon: <Users /> },
+    { label: 'Moments', icon: <Camera /> },
+    { label: 'Messages', icon: <MessagesSquare /> },
+  ];
+
+  const socialStats = [
+    { label: 'Followers', count: 46 },
+    { label: 'Following', count: 25 },
+    { label: 'Moments', count: 2 },
+  ];
+
+  return (
+    <div
+      className={`px-10 flex flex-col items-center space-y-5 ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}
+    >
+      <div className='flex flex-col items-center mt-10'>
+        <img
+          src='./profile-picture.jpg'
+          className='object-cover w-23 h-23 rounded-full border border-gray-500 p-1'
+        />
+        <h1 className='text-white text-2xl'>Jennifer Lawrence</h1>
+        <p className='text-xs'>@johndoe27</p>
+      </div>
+      <div className='flex justify-center space-x-5 w-62 p-3 rounded-md'>
+        {socialStats.map(({ label, count }) => (
+          <div key={label}>
+            <h1 className='text-white text-center font-black'>{count}</h1>
+            <p className='text-xs'>{label}</p>
+          </div>
+        ))}
+      </div>
+      <div className='space-x-5 w-62 bg-[#0E0E0E] p-3 rounded-md'>
+        <ul className='space-y-3'>
+          {sidebarLinks.map(({ label, icon }) => (
+            <li
+              key={label}
+              className='hover:bg-[#1b1b1b] cursor-pointer rounded-md'
+            >
+              <a className='flex items-center space-x-2 py-5 pl-2'>
+                {icon}
+                <p className='text-sm'>{label}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button className='flex items-center bg-accent hover:bg-[#ff4b4e] text-sm py-2 px-3 mt-5 rounded-md'>
+        <CirclePlus size={20} className='mr-2' />
+        Create a Moment
+      </button>
+    </div>
+  );
+};
+
+export default Sidebar;
--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,21 +1,36 @@
-import DashboardNavbar from "../../components/layout/DashboardNavbar";
+import DashboardNavbar from '../../components/layout/DashboardNavbar';
+import Sidebar from '../../components/layout/Sidebar';
+import { useState } from 'react';
+import { Menu, X } from 'lucide-react';
+import Loading from '../../components/ui/Loading';
 
-const Dashboard = ( { user, error, setUser } ) => {
-  return (
+const Dashboard = ({ user, error, setUser }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return user ? (
     <>
-    <DashboardNavbar user={user} setUser={setUser} error={error} />
-    <section>
-      {error && <p className='text-red-400 text-xs mt-2 italic'>{error}</p>}
-      {user ? (
-        <div className="max-w-7xl flex lg:mx-auto px-6">
-          <h2>Welcome, {user.username}</h2>
-          <p>{user.email}</p>
-          </div> )
-          : <div></div>
-         }
-    </section>
+      <DashboardNavbar user={user} setUser={setUser} error={error} />
+      <section className='w-full flex h-screen '>
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div className='flex-1'>This is the main content</div>
+        {sidebarOpen ? (
+          <button
+            onClick={() => setSidebarOpen(false)}
+            className='sm:hidden absolute top-4 right-4 z-50 p-2 bg-[#0E0E0E] rounded-md text-white hover:bg-[#1b1b1b] transition-colors duration-200'
+          >
+            <X size={24} />
+          </button>
+        ) : (
+          <button
+            onClick={() => setSidebarOpen(true)}
+            className='sm:hidden absolute top-4 right-4 z-50 p-2 bg-[#0E0E0E] rounded-md text-white hover:bg-[#1b1b1b] transition-colors duration-200'
+          >
+            <Menu size={24} />
+          </button>
+        )}
+      </section>
     </>
-  )
+  ) : <Loading />
 };
 
 export default Dashboard;
